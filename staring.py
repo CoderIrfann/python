a69213b84030aa7c0025a7f5c4cbb1cc391e16d9
Coder Irfan <mohdirfan70097@gmail.com>
2025-01-14 11:36:31 +0530

    Creating a first program in python add two number 14 Jan 2025

---
+++ staring.py
@@ -0,0 +1,6 @@
+# // creating a program to add two number
+
+fisrtNumber = 2;
+secondNumber = 4;
+result = fisrtNumber+secondNumber;
+print("Sum of" , fisrtNumber ,"and", secondNumber ,"is = " , result   )
