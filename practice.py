453e15d2fa4365a74e0a1be5e99985038eec5032
Coder Irfan <mohdirfan70097@gmail.com>
2025-01-31 15:23:58 +0530

    Updating some error

--- practice.py
+++ practice.py
@@ -1,49 +1,51 @@
-# # # # factorial number
-# # # a = int(input("Enter your number : "))
+# # # # # factorial number
+# # # # a = int(input("Enter your number : "))
+# # # #
+# # # # factorial = 1
+# # # #
+# # # # for x in range(11):
+# # # #     factorial =a* x
+# # # #
+# # # #     print("The factorial is : " , factorial)
+# # # #
+# # # #
+# # # # check number is positive or not
 # # #
-# # # factorial = 1
+# # # num1 = int(input("Enter your number :"))
 # # #
-# # # for x in range(11):
-# # #     factorial =a* x
-# # #
-# # #     print("The factorial is : " , factorial)
-# # #
-# # #
-# # # check number is positive or not
+# # # if(num1 % 2 == 0):
+# # #     print(num1," ,Is Even")
+# # # else:
+# # #     print(num1 ," ,Is Odd")
 # #
-# # num1 = int(input("Enter your number :"))
+# # # Simple calculator
+# # num1 = int(input("Enter first number :"))
+# # num2 = int(input("Enter second number :"))
 # #
-# # if(num1 % 2 == 0):
-# #     print(num1," ,Is positive")
-# # else:
-# #     print(num1 ," ,Is Negative")
+# # # Calculator logic operation
+# # sum = num1+num2
+# # sub = num1-num2
+# # multiply = num1*num2
+# # divide = num1%num2
+# # power = num1**num2
+# #
+# # # printing the operation
+# # print("Addition of",num1 , "+" , num2 , "is :" , sum)
+# # print("Subtraction of",num1 , "-" , num2 , "is :" , sub)
+# # print("Multiplication of",num1 , "*" , num2 , "is :" , multiply)
+# # print("Division of",num1 , "%" , num2 , "is :" , divide)
+# # print("Power of",num1 , "**" , num2 , "is :" , power)
 #
-# # Simple calculator
-# num1 = int(input("Enter first number :"))
-# num2 = int(input("Enter second number :"))
+# # reverse string
+# # reverseString = "Mohd Irfan"[::-1]
+# # print(reverseString)
 #
-# # Calculator logic operation
-# sum = num1+num2
-# sub = num1-num2
-# multiply = num1*num2
-# divide = num1%num2
-# power = num1**num2
+# # convert a string to uppercase and lowercase
 #
-# # printing the operation
-# print("Addition of",num1 , "+" , num2 , "is :" , sum)
-# print("Subtraction of",num1 , "-" , num2 , "is :" , sub)
-# print("Multiplication of",num1 , "*" , num2 , "is :" , multiply)
-# print("Division of",num1 , "%" , num2 , "is :" , divide)
-# print("Power of",num1 , "**" , num2 , "is :" , power)
+# UpperCaseStringConvertor = "hi , i am mohd irfan"
+# x = UpperCaseStringConvertor.upper()
+# y = UpperCaseStringConvertor.lower()
+# print(x)
+# print(y)
 
-# reverse string
-# reverseString = "Mohd Irfan"[::-1]
-# print(reverseString)
-
-# convert a string to uppercase and lowercase
-
-UpperCaseStringConvertor = "hi , i am mohd irfan"
-x = UpperCaseStringConvertor.upper()
-y = UpperCaseStringConvertor.lower()
-print(x)
-print(y)
+# // reverse given number
