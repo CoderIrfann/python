16f90ef727d7f9edeb9695e75aae6b738a696378
Coder Irfan <mohdirfan70097@gmail.com>
2025-01-31 14:44:05 +0530

    Addning new program called reverse string

--- practice.py
+++ practice.py
@@ -1,37 +1,41 @@
-# # # factorial number
-# # a = int(input("Enter your number : "))
+# # # # factorial number
+# # # a = int(input("Enter your number : "))
+# # #
+# # # factorial = 1
+# # #
+# # # for x in range(11):
+# # #     factorial =a* x
+# # #
+# # #     print("The factorial is : " , factorial)
+# # #
+# # #
+# # # check number is positive or not
 # #
-# # factorial = 1
+# # num1 = int(input("Enter your number :"))
 # #
-# # for x in range(11):
-# #     factorial =a* x
-# #
-# #     print("The factorial is : " , factorial)
-# #
-# #
-# # check number is positive or not
+# # if(num1 % 2 == 0):
+# #     print(num1," ,Is positive")
+# # else:
+# #     print(num1 ," ,Is Negative")
 #
-# num1 = int(input("Enter your number :"))
+# # Simple calculator
+# num1 = int(input("Enter first number :"))
+# num2 = int(input("Enter second number :"))
 #
-# if(num1 % 2 == 0):
-#     print(num1," ,Is positive")
-# else:
-#     print(num1 ," ,Is Negative")
+# # Calculator logic operation
+# sum = num1+num2
+# sub = num1-num2
+# multiply = num1*num2
+# divide = num1%num2
+# power = num1**num2
+#
+# # printing the operation
+# print("Addition of",num1 , "+" , num2 , "is :" , sum)
+# print("Subtraction of",num1 , "-" , num2 , "is :" , sub)
+# print("Multiplication of",num1 , "*" , num2 , "is :" , multiply)
+# print("Division of",num1 , "%" , num2 , "is :" , divide)
+# print("Power of",num1 , "**" , num2 , "is :" , power)
 
-# Simple calculator
-num1 = int(input("Enter first number :"))
-num2 = int(input("Enter second number :"))
-
-# Calculator logic operation
-sum = num1+num2
-sub = num1-num2
-multiply = num1*num2
-divide = num1%num2
-power = num1**num2
-
-# printing the operation
-print("Addition of",num1 , "+" , num2 , "is :" , sum)
-print("Subtraction of",num1 , "-" , num2 , "is :" , sub)
-print("Multiplication of",num1 , "*" , num2 , "is :" , multiply)
-print("Division of",num1 , "%" , num2 , "is :" , divide)
-print("Power of",num1 , "**" , num2 , "is :" , power)
+# reverse string
+# reverseString = "Mohd Irfan"[::-1]
+# print(reverseString)
