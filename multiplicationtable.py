54333a65801824234a475c750c20ddc99f4e5782
Coder Irfan <mohdirfan70097@gmail.com>
2025-02-07 14:30:07 +0530

    Solving problem

---
+++ multiplicationtable.py
@@ -0,0 +1,20 @@
+# # #  # Genearte multiplication table of a number
+# # #
+# # # a = int(input("Enter your number : "))
+# # #
+# # # for x in range(11) :
+# # #     print(a ,"x" ,x ,"=" ,a*x)
+# #
+# # age = int(input("Enter your age"))
+# #
+# # if(age <= 18):
+# #     print("You are children you cannot vote..")
+# # else:
+# #     print("You are eligibale for vote..")
+#
+#
+# for x in range(11):
+#     if(x == 6):
+#        continue
+#     else:
+#          print(x)
